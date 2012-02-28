990ed3a1fa0f8ee6aa1b6c64ef7e76032e13dc9c
Charles_Stain <alfredo.dinapoli@gmail.com>
2012-02-28 15:31:54 +0100

    First

---
+++ MogreSimpleApp/Program.fs
@@ -0,0 +1,19 @@
+﻿// Ulteriori informazioni su F# all'indirizzo http://fsharp.net
+
+open Mogre
+open Mogre.TutorialFramework
+open System
+
+type SimpleApp() =
+    inherit BaseApplication()
+
+    override this.CreateScene() =
+        this.mSceneMgr.AmbientLight <- new ColourValue(1.0f, 1.0f, 1.0f)
+        let ent = this.mSceneMgr.CreateEntity("Head", "ogrehead.mesh")
+        let node = this.mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode")
+        node.AttachObject(ent)
+
+
+
+let app = new SimpleApp()
+app.Go()
